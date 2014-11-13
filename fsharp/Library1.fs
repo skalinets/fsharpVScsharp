7a558fe39a9fbde820230f8dec051eee7e4db2d3
Serhiy Kalinets <kalinets@gmail.com>
2014-11-13 12:15:24 +0200

    initial commit

---
+++ fsharp/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace fsharp
+
+type Class1() = 
+    member this.X = "F#"
---
+++ fsharp/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Library1.fs"
+open fsharp
+
+// Define your library scripting code here
+
