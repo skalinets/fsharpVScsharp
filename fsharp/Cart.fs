9b6646c28550329cb4543c06eba5c872e366390d
Serhiy Kalinets <kalinets@gmail.com>
2014-11-13 13:28:27 +0200

    adds some tests

---
+++ fsharp/Cart.fs
@@ -0,0 +1,18 @@
+﻿module Cart
+
+type Item = {Name: string; Price: double}
+type Cart = {Items: Item list}
+
+let milk = {Name = "milk"; Price = 15.0}
+
+let createEmpty () =
+    {Items = []}
+
+let add i a c = 
+    let is = List.replicate a i
+    { c with Items = is @ c.Items }
+
+let getTotal c = 
+    c.Items 
+        |> List.map (fun i -> i.Price)
+        |> List.fold (+) 0.0
---
+++ fsharp/Tests.fs
@@ -0,0 +1,17 @@
+﻿module fsharp.Tests
+
+open NUnit.Framework
+open FsUnit
+open Cart
+    
+[<Test>]
+let ``just simple test`` () = 1 |> should equal 1
+
+[<Test>]
+let ``should create empty cart`` () = createEmpty().Items |> should be Empty
+
+[<Test>]
+let ``should calculate sum or all orders`` () =
+    let cart = createEmpty () |> add milk 2
+    let t = getTotal cart
+    t |> should equal 30.0
