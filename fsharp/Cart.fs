c7532f0747285070e49fd0a3257d2b5182a7a6e6
Serhiy Kalinets <kalinets@gmail.com>
2014-11-13 15:06:19 +0200

    adds 3rd free milk case

--- fsharp/Cart.fs
+++ fsharp/Cart.fs
@@ -1,18 +1,36 @@
 ﻿module Cart
 
-type Item = {Name: string; Price: double}
-type Cart = {Items: Item list}
+type Item = {Name: string; Price: float}
+type Cart = {Items: Item list; DiscountCalculators : (Cart -> float) list}
 
-let milk = {Name = "milk"; Price = 15.0}
+let milk = {Name = "Milk"; Price = 15.0}
+
+let sum = List.fold (+) 0.0
+
+let totalWihoutDiscounts c = 
+    let getPrice i = i.Price
+    c.Items 
+        |> List.map getPrice
+        |> sum
 
 let createEmpty () =
-    {Items = []}
+    {Items = []; DiscountCalculators = []}
 
 let add i a c = 
     let is = List.replicate a i
     { c with Items = is @ c.Items }
 
 let getTotal c = 
-    c.Items 
-        |> List.map (fun i -> i.Price)
-        |> List.fold (+) 0.0
+    let allDiscounts = c.DiscountCalculators 
+                        |> List.map (fun cal -> cal c)
+                        |> sum
+    totalWihoutDiscounts c - allDiscounts 
+
+let applyDiscount d c = { c with DiscountCalculators = d :: c.DiscountCalculators }
+
+let nthIsFree n i c =
+    i.Price * (((c.Items |> List.filter ((=) i) |> List.length) / n) |> float)
+    
+let thirdIsFree = nthIsFree 3
+
+let thirdMilkIsFree = thirdIsFree milk
--- fsharp/Tests.fs
+++ fsharp/Tests.fs
@@ -12,6 +12,15 @@ let ``should create empty cart`` () = createEmpty().Items |> should be Empty
 
 [<Test>]
 let ``should calculate sum or all orders`` () =
-    let cart = createEmpty () |> add milk 2
-    let t = getTotal cart
-    t |> should equal 30.0
+    createEmpty () |> add milk 4 
+        |> getTotal |> should equal 60.0
+
+[<Test>]
+let ``3rd milk should be free`` () =
+    createEmpty () |> add milk 3 |> applyDiscount thirdMilkIsFree
+        |> getTotal |> should equal 30.0
+
+[<Test>]
+let ``2 milks shoud be free if there 6 in the cart`` () =
+    createEmpty () |> add milk 6 |> applyDiscount thirdMilkIsFree
+        |> getTotal |> should equal (milk.Price * 4.0)
